824afa56d48e0d4a13892538a07828769169c5cd
swarupkharul <swarupkharulsk@gmail.com>
2021-09-29 23:47:31 +0530

    feat: add error messages in UI

--- accounts/static/frontend/js/pin-login.js
+++ accounts/static/frontend/js/pin-login.js
@@ -1,44 +1,52 @@
 function pincode() {
+
     var input = document.getElementById("userInput").value;
-    console.log(document.getElementById("userInput").type)
+    var input2 = document.getElementById("userInput2").value;
     fdata = new FormData(document.getElementById("PINform"));
     console.log(fdata)
-    fetch("setpattern/", {
-        method: "POST",
-        body: fdata,
-    })
-        .then((res) => res.json()) // note that the `status` function is actually **called** here, and that it **returns a promise***
-        .then((res) => {
-            console.log(res.Success);
-            let save = document.getElementById("showPINSuccessResult");
-            let fail = document.getElementById("showPINFailResult");
-            // ... which is why `res` shows up here as the first parameter to the anonymous function
-            if (res.Success) {
-                save.style.display = "block";
-                save.innerHTML += res.Message;
-                fail.style.display = "none";
-            } else {
-                fail.style.display = "block";
-                fail.innerHTML += res.Message;
-                save.style.display = "none";
-            }
+    if (input === input2) {
+        fetch("setpattern/", {
+            method: "POST",
+            body: fdata,
         })
-        .catch((error) => {
-            console.log("Request failed", error);
-        });
+            .then((res) => res.json()) // note that the `status` function is actually **called** here, and that it **returns a promise***
+            .then((res) => {
+                console.log(res.Success);
+                let save = document.getElementById("showPINSuccessResult");
+                let fail = document.getElementById("showPINFailResult");
+                // ... which is why `res` shows up here as the first parameter to the anonymous function
+                if (res.Success) {
+                    save.style.display = "block";
+                    save.getElementById("tick2").innerHTML = res.Message;
+                    fail.style.display = "none";
+                } else {
+                    fail.style.display = "block";
+                    fail.getElementById("cross2").innerHTML = res.Message;
+                    save.style.display = "none";
+                }
+            })
+            .catch((error) => {
+                console.log("Request failed", error);
+            });
+    }
+    else {
+        alert("PINs do not match")
+    }
+
 }
 function showPassword() {
-    // document.getElementById("userInput").type = "text";
+
     var eye = document.getElementById("eyeb");
+    var span = document.getElementById("eyespan")
     if (document.getElementById("userInput").type === "password") {
-        //     // eye.innerHTML=="&#xf070;" eye.innerHTML="&#xf06e;";
-        //     alert("hi");
+        //span.innerHTML=='<i style="color: #eee" id="eyeb" class="fa">&#xf070;</i></span>';
+
         document.getElementById("userInput").type = "text";
 
 
     }
     else {
-        document.getElementById("eyeb").innerText = "&#xf06e;"
+        //span.innerHTML = '<i style="color: #eee" id="eyeb" class="fa">&#xf06e;</i></span>';
         document.getElementById("userInput").type = "password";
 
     }
@@ -46,41 +54,3 @@ function showPassword() {
 
 }
 
-// jQuery.noConflict();
-// $(document).ready(function (e) {
-//     $(function () {
-//         $("input[name='password']").on('input', function (e) {
-//             $(this).val($(this).val().replace(/[^0-9]/g, ''));
-//         });
-//     });
-
-
-
-
-    // function match(){
-    //     var og= document.getElementById("userInput").value;
-    //     var conf= document.getElementById("userInput2").value;
-    //     if (og===conf){
-    //         console.log(true);
-    //     }
-    //     else{
-    //         console.log("not working");
-    //     }
-    // }
-    // $('#userInput, #userInput2').on('keyup', function () {
-    //     if ($('#userInput').val() == $('#userInput2').val()) {
-    //         $('#message').html('').css('color', '#eee');
-    //     } else
-    //         $('#message').html('pins do not match! ').css('color', '#eee');
-    // });
-    // $(".showpass").click(function () {
-    //     $(this).toggleClass("fa-eye fa-eye-slash");
-    //     input = $("#userInput");
-    //     if (input.attr("type") == "password") {
-    //         input.attr("type", "text");
-    //     } else {
-    //         input.attr("type", "password");
-    //     }
-    // });
-// }
-// )
